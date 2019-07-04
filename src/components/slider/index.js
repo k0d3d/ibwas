ccc9ca36ee3aa9e6db3c2427675a5d0b02e1391d
Koded <michael@koded.com>
2019-07-04 11:42:21 +0100

    using css

--- src/components/slider/index.js
+++ src/components/slider/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faAngleDoubleDown } from '@fortawesome/free-solid-svg-icons'
+import Anime from 'react-anime'
 
 
 import './index.scss'
@@ -13,12 +14,13 @@ const Slider = () => (
             <div className="carousel-item active">
                 <div className="container">
                     <aside>
-                        <h1>
-                            Open the door to a world of the best furniture materials.
-                        </h1>
-                        <p>
-                            Whatever your wood furniture needs, you can order quality materials from us. 
-                        </p>
+                            <h1 class="slide-in-blurred-top">
+                                Open the door to a world of the best furniture materials.
+                            </h1>
+
+                            <p>
+                                Whatever your wood furniture needs, you can order quality materials from us. 
+                            </p>
                     </aside>
                 </div>
             </div>
